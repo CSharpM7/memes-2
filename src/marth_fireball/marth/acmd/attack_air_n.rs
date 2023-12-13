e53e2151f7788898918ba789f3257c20bc8c64fa
C# <afr0man005@gmail.com>
2023-12-12 19:29:08 -0800

    Update attack_air_n.rs

--- src/marth_fireball/marth/acmd/attack_air_n.rs
+++ src/marth_fireball/marth/acmd/attack_air_n.rs
@@ -4,6 +4,7 @@ use crate::marth_fireball::imports::*;
 unsafe extern "C" fn game_attackairn(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 2.0);
     if macros::is_excute(agent) {
+        println!("Fireball!");
         ArticleModule::generate_article(agent.module_accessor, FIGHTER_MARTH_GENERATE_ARTICLE, false, -1);
     }
 }
