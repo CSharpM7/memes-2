54127eca8ca1e1006db9c9853e96599b14643289
C# <afr0man005@gmail.com>
2024-11-07 14:02:50 -0800

    Update cliff_attack.rs

--- src/ledge_special/common/status/cliff_attack.rs
+++ src/ledge_special/common/status/cliff_attack.rs
@@ -7,11 +7,9 @@ pub unsafe extern "C" fn cliff_attack_main(fighter: &mut L2CFighterCommon) -> L2
     || ControlModule::check_button_on(fighter.module_accessor, *CONTROL_PAD_BUTTON_SPECIAL_RAW){
         println!("Special");
         MotionModule::change_motion(fighter.module_accessor, Hash40::new("cliff_special_quick"), 0.0, 1.0, false, 0.0, false, false);
+        //return fighter.sub_shift_status_main(L2CValue::Ptr(cliff_attack_main_loop as *const () as _));
     }
-    else {
-        MotionModule::change_motion(fighter.module_accessor, Hash40::new("cliff_attack_quick"), 0.0, 1.0, false, 0.0, false, false);
-    }
-    fighter.sub_shift_status_main(L2CValue::Ptr(cliff_attack_main_loop as *const () as _))
+    fighter.sub_shift_status_main(L2CValue::Ptr(L2CFighterCommon_status_CliffAttack_Main as *const () as _))
 }
 pub unsafe extern "C" fn cliff_attack_main_loop(fighter: &mut L2CFighterCommon) -> L2CValue {
     return fighter.status_CliffAttack_Main();
